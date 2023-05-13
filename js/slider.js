9e0886b20822c12752df1a1b1bfb578685af85ac
GitM4n <95144849+GitM4n@users.noreply.github.com>
2023-05-13 16:10:30 +0300

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -3,79 +3,70 @@ const sliders = document.querySelectorAll('.slider');
 
 
 
-sliders.forEach( slider =>{
-  const container =  slider.querySelector('.slider__container');
-  const slides = container.querySelectorAll('.slider__items img')
-  const prevBtn = slider.querySelector('.prev-btn');
-  const nextBtn = slider.querySelector('.next-btn');
+  sliders.forEach( slider =>{
+    const container =  slider.querySelector('.slider__container');
+    const slides = container.querySelectorAll('.slider__items img')
+    const prevBtn = slider.querySelector('.prev-btn');
+    const nextBtn = slider.querySelector('.next-btn');
+    setTimeout(()=>{
+      let slideIndex = 0;
+      let slideWidth = slides[0].clientWidth;
+      let progress;
+      let progBarTimer = setInterval(showNextSlide, 5000);
 
 
-  let slideIndex = 0;
-  let slideWidth;
-  setTimeout(()=>{
-    slideWidth = slides[0].clientWidth;
-  }, 500);
-  
-  
-  function updateSlider(){
-    container.style.transform = `translateX(-${slideIndex * slideWidth}px)`;
-   
-  }
+      function AnimateBar(){
+        const progressBar = slider.querySelector('.progress-bar__bg');
+        progressBar.style.animationName = 'none';
+        setTimeout(()=>{progressBar.style.animationName = 'progressWidth';}, 0)
 
-function showPrevSlide() {
-  slideIndex--;
-    if (slideIndex < 0) {
-      slideIndex = slides.length - 1;
+      
     }
-    
-    updateSlider();
-   
-  }
-  
-  function showNextSlide() {
-    slideIndex++;
-    if (slideIndex > slides.length - 1) {
-      slideIndex = 0;
-    }
-    updateSlider();
-  }
 
+    function showPrevSlide() {
+      slideIndex--;
+        if (slideIndex < 0) {
+          slideIndex = slides.length - 1;
+        }
+        container.style.transform = `translateX(-${slideIndex * slideWidth}px)`;
+        
+      
+      }
+      
+      function showNextSlide() {
+        slideIndex++;
+        if (slideIndex > slides.length - 1) {
+          slideIndex = 0;
+        }
+        container.style.transform = `translateX(-${slideIndex * slideWidth}px)`;
+      }
 
-  let progBarTimer = setInterval(showNextSlide, 5000);
- 
-
-
-  window.addEventListener('resize', () => {
-    slideWidth = slides[0].clientWidth;
-    slideIndex = 0; //возвращение к 1-му слайду при изменении ширины окна браузера
-    updateSlider();
-   
-  });
-
-  function resetProgressBar(){
-    const progBar = document.querySelectorAll('.progress-bar__bg');
-    progBar.forEach((item)=>{
-      item.style.animationName = 'none';
-      setTimeout(()=>{item.style.animationName = 'progressWidth';}, 10)
-    });   
-  };
-
-  prevBtn.addEventListener('click', ()=>{
+    prevBtn.addEventListener('click', ()=>{
       showPrevSlide();
-      resetProgressBar();
       clearInterval(progBarTimer);
       progBarTimer = setInterval(showNextSlide, 5000);
-    });
-  
+      progress = 0;
+      AnimateBar();
 
-  nextBtn.addEventListener('click', ()=>{
+    })
+
+    nextBtn.addEventListener('click', ()=>{
       showNextSlide();
-      resetProgressBar();
       clearInterval(progBarTimer);
       progBarTimer = setInterval(showNextSlide, 5000);
-    });
-  
+      progress = 0;
+      AnimateBar();
 
+    })
+    window.addEventListener('resize', () => {
+          slideWidth = slides[0].clientWidth;
+          container.style.transform = `translateX(-${slideIndex * slideWidth}px)`;
+      
+        
+    });
+    AnimateBar();  
+  },500);
+  
 })
 
 
